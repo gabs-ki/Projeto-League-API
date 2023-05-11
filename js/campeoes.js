61efcf3e2aa9e536d4ee8db256a3182aae22f829
marceloGabriel <log4304.016@gmail.com>
2023-05-10 23:14:06 -0300

    Configurando funções puras

--- js/campeoes.js
+++ js/campeoes.js
@@ -3,14 +3,18 @@
 import "./router.js"
 import { linguas } from "./tradução/classes.js"
 
-// const idCampeao = window.localStorage.getItem('campeao')
-
-// const campeaoApi = await pegarCampeaoApi(idCampeao)
-
 
 export const carregarCampeoes = async (nome, dados) => {
 
+    const splahChamp = document.createElement('div')
+    splahChamp.id = 'tela__splash__id'
+   
     const container = document.getElementById('container')
+
+    container.append (
+        splahChamp
+    )
+
     const champion = document.getElementById('champion')
     const navegador = document.getElementById('navegacaoPosterior')
 
@@ -33,11 +37,7 @@ export const carregarCampeoes = async (nome, dados) => {
             }
 
         })
-
-        
-        
-
-        
+   
         conteudoCampeoes.imageIconeCampeao = `http://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/${nome}.png`
  
         const classe = ((dados[0][1].tags[0]).toString()).toLowerCase()
@@ -46,90 +46,6 @@ export const carregarCampeoes = async (nome, dados) => {
   
 
     //
-
-    const telaSplashArt = document.createElement('div')
-    telaSplashArt.classList.add('tela__splash__art__removida')
-    
-    //Botão superior 
-
-        const navegadorSuperiorSplash = document.createElement('div')
-        navegadorSuperiorSplash.classList.add('navegador__superior__splash')
-
-        
-        const containerBotaoSuperior = document.createElement('div')
-        containerBotaoSuperior.classList.add('container__botao__superior')
-
-
-
-        const botaoSuperior = document.createElement('ion-icon')
-        botaoSuperior.setAttribute('name','chevron-down-outline')
-        botaoSuperior.classList.add('botao__superior')
-
-        const containerIconeBotaoSuperior = document.createElement('div')
-        containerIconeBotaoSuperior.classList.add('container__icone__botao__superior')
-
-        containerIconeBotaoSuperior.appendChild (
-            botaoSuperior
-        )
-
-        containerIconeBotaoSuperior.addEventListener('click', () => {
-            telaSplashArt.classList.remove('tela__splash__art')
-            telaSplashArt.classList.add('tela__splash__art__removida')
-        })
-
-        containerBotaoSuperior.append (
-            navegadorSuperiorSplash,
-            containerIconeBotaoSuperior
-        )
-
-    //
-
-    const containerSplashCampeao = document.createElement('div')
-    containerSplashCampeao.classList.add('container__splash__campeao')
-
-    const tituloSplashCampeao = document.createElement('div')
-    tituloSplashCampeao.classList.add('titulo__splash__campeao')
-
-    const nomeSplashCampeao = document.createElement('h3')
-    nomeSplashCampeao.classList.add('nome__splash__campeao')
-    nomeSplashCampeao.textContent = ((dados[0][1].name).toString()).toUpperCase()
-
-    const subNomeSplashCampeao = document.createElement('p')
-    subNomeSplashCampeao.classList.add('sub__nome__splash__campeao')
-    subNomeSplashCampeao.textContent = ((dados[0][1].title).toString()).toUpperCase()
-        
-    tituloSplashCampeao.append (
-        nomeSplashCampeao,
-        subNomeSplashCampeao
-    )
-
-    const molduraSplashCampeao = document.createElement('img')
-    molduraSplashCampeao.classList.add('moldura__splash__campeao')
-    molduraSplashCampeao.src = './img/splashAccessory.svg'
-
-    const imageSplashCampeao = document.createElement('img')
-    imageSplashCampeao.classList.add('image__splash__campeao')
-    imageSplashCampeao.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${nome}_0.jpg`
-
-    containerSplashCampeao.append (
-        tituloSplashCampeao,
-        molduraSplashCampeao,
-        imageSplashCampeao
-    )
-
-
-    //
-
-    const navegadorPosteriorSplash = document.createElement('div')
-
-
-    //
-
-    telaSplashArt.append (
-        containerBotaoSuperior,
-        containerSplashCampeao,
-        navegadorPosteriorSplash
-    )
 
     //
 
@@ -160,8 +76,13 @@ export const carregarCampeoes = async (nome, dados) => {
 
     // Evento para mostrar a splash art do campeão
     molduraCampeao.addEventListener('click', () => {
-        telaSplashArt.classList.remove('tela__splash__art__removida')
-        telaSplashArt.classList.add('tela__splash__art')
+        splahChamp.classList.remove('testeB')
+        splahChamp.classList.add('testeA')
+
+        
+        splahChamp.replaceChildren(
+            gerarTelaSplash(dados,nome)
+        )
     })
 
    
@@ -218,8 +139,7 @@ export const carregarCampeoes = async (nome, dados) => {
     )
 
     container.append (
-        navegador,
-        telaSplashArt
+        navegador
     )
  
     // const linkMenu = document.createElement('a')
@@ -235,16 +155,221 @@ export const carregarCampeoes = async (nome, dados) => {
     return container
 }
 
+const gerarTelaHabilidades = (dados, nome) => {
+
+    const telaSplashArt = document.createElement('div')
+    telaSplashArt.classList.add('tela__habilidades')
+    telaSplashArt.id = 'tela__splash__art'
+    
+    //Botão superior 
+
+        const navegadorSuperiorSplash = document.createElement('div')
+        navegadorSuperiorSplash.classList.add('navegador__superior__skill')
+
+        const containerBotaoSuperior = document.createElement('div')
+        containerBotaoSuperior.classList.add('container__botao__superior')
+
+        const botaoSuperior = document.createElement('ion-icon')
+        botaoSuperior.setAttribute('name','chevron-down-outline')
+        botaoSuperior.classList.add('botao__superior')
+
+        const containerIconeBotaoSuperior = document.createElement('div')
+        containerIconeBotaoSuperior.classList.add('container__icone__botao__superior')
+
+        containerIconeBotaoSuperior.appendChild (
+            botaoSuperior
+        )
+
+        containerIconeBotaoSuperior.addEventListener('click', () => {
+            const tela = document.getElementById('tela__splash__art')
+            tela.classList.remove('tela__splash__art')
+            tela.classList.add('tela__splash__art__removida')
+        })
+
+        containerBotaoSuperior.append (
+            navegadorSuperiorSplash,
+            containerIconeBotaoSuperior
+        )
+
+    //
+
+    const containerSplashCampeao = document.createElement('div')
+    containerSplashCampeao.classList.add('container__splash__campeao')
 
 
+    const navegadorPosteriorSplash = document.createElement('div')
+    navegadorPosteriorSplash.classList.add('navegador__posterior__splash')
+
+    //
+    const ancoraSkin = document.createElement('div')
+    ancoraSkin.classList.add('ancora__skin__splash')
+
+    const ancoraChamp = document.createElement('div')
+    ancoraChamp.classList.add('ancora__habilidades__splash')
+    ancoraChamp.addEventListener('click', () => {
+        
+    })
+
+    const iconeSkin = document.createElement('img')
+    iconeSkin.classList.add('icone__skin__splash')
+    iconeSkin.src = `./img/iconSkins.svg`
 
 
+    ancoraSkin.append (
+        iconeSkin
+    )
 
-// const cards = Object.entries(await campeaoApi)
-// const testeA = Object.entries(cards[3][1])
+    navegadorPosteriorSplash.append (
+        ancoraSkin
+    )
 
-// const gerarItems = testeA.map(teste)
+
+    //
+
+    telaSplashArt.append (
+        containerBotaoSuperior,
+        containerSplashCampeao,
+        navegadorPosteriorSplash
+    )
+
+    console.log(telaSplashArt)
+
+    return telaSplashArt
+
+}
+
+const gerarTelaSplash = (dados, nome) => {
+
+    const telaSplashArt = document.createElement('div')
+    telaSplashArt.classList.add('tela__splash__art')
+    telaSplashArt.id = 'tela__splash__art'
+    
+    //Botão superior 
+
+        const navegadorSuperiorSplash = document.createElement('div')
+        navegadorSuperiorSplash.classList.add('navegador__superior__splash')
+
+        
+        const containerBotaoSuperior = document.createElement('div')
+        containerBotaoSuperior.classList.add('container__botao__superior')
+
+        const botaoSuperior = document.createElement('ion-icon')
+        botaoSuperior.setAttribute('name','chevron-down-outline')
+        botaoSuperior.classList.add('botao__superior')
+
+        const containerIconeBotaoSuperior = document.createElement('div')
+        containerIconeBotaoSuperior.classList.add('container__icone__botao__superior')
+
+        containerIconeBotaoSuperior.appendChild (
+            botaoSuperior
+        )
+
+        containerIconeBotaoSuperior.addEventListener('click', () => {
+            const getTelaSplash = document.getElementById('tela__splash__id')
+            
+            getTelaSplash.classList.add('testeA')
+            getTelaSplash.classList.add('testeB')
+
+            telaSplashArt.classList.remove('tela__splash__art')
+            telaSplashArt.classList.add('tela__splash__art__removida')
+        })
+
+        containerBotaoSuperior.append (
+            navegadorSuperiorSplash,
+            containerIconeBotaoSuperior
+        )
+
+    //
+
+    const containerSplashCampeao = document.createElement('div')
+    containerSplashCampeao.classList.add('container__splash__campeao')
+
+    const tituloSplashCampeao = document.createElement('div')
+    tituloSplashCampeao.classList.add('titulo__splash__campeao')
+
+    const nomeSplashCampeao = document.createElement('h3')
+    nomeSplashCampeao.classList.add('nome__splash__campeao')
+    nomeSplashCampeao.textContent = ((dados[0][1].name).toString()).toUpperCase()
+
+    const subNomeSplashCampeao = document.createElement('p')
+    subNomeSplashCampeao.classList.add('sub__nome__splash__campeao')
+    subNomeSplashCampeao.textContent = ((dados[0][1].title).toString()).toUpperCase()
+        
+    tituloSplashCampeao.append (
+        nomeSplashCampeao,
+        subNomeSplashCampeao
+    )
+
+    const molduraSplashCampeao = document.createElement('img')
+    molduraSplashCampeao.classList.add('moldura__splash__campeao')
+    molduraSplashCampeao.src = './img/splashAccessory.svg'
+
+    const imageSplashCampeao = document.createElement('img')
+    imageSplashCampeao.classList.add('image__splash__campeao')
+    imageSplashCampeao.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${nome}_0.jpg`
+
+    containerSplashCampeao.append (
+        tituloSplashCampeao,
+        molduraSplashCampeao,
+        imageSplashCampeao
+    )
 
 
-// container.replaceChildren(...gerarItems)
+    //
+
+    const navegadorPosteriorSplash = document.createElement('div')
+    navegadorPosteriorSplash.classList.add('navegador__posterior__splash')
+
+    //
+    const ancoraSkin = document.createElement('div')
+    ancoraSkin.classList.add('ancora__skin__splash')
+
+    const ancoraHabilidades = document.createElement('div')
+    ancoraHabilidades.classList.add('ancora__habilidades__splash')
+    ancoraHabilidades.addEventListener('click', () => {
+        
+        const teste = gerarTelaHabilidades(dados,nome)
+
+        telaSplashArt.replaceChildren(
+            teste
+        )
+    })
+
+    const iconeSkin = document.createElement('img')
+    iconeSkin.classList.add('icone__skin__splash')
+    iconeSkin.src = `./img/iconSkins.svg`
+
+    const iconeHabilidades = document.createElement('img')
+    iconeHabilidades.classList.add('icone__habilidades__splash')
+    iconeHabilidades.src = `./img/iconHabilidades.svg`
+
+    ancoraHabilidades.append (
+        iconeHabilidades
+    )
+
+    ancoraSkin.append (
+        iconeSkin
+    )
+
+    navegadorPosteriorSplash.append (
+        ancoraSkin,
+        ancoraHabilidades
+    )
+
+
+    //
+
+    telaSplashArt.append (
+        containerBotaoSuperior,
+        containerSplashCampeao,
+        navegadorPosteriorSplash
+    )
+
+    
+  
+
+    return telaSplashArt
+}
+
+
 
